ea5198bfa3bfef070713b9b39985cffc92b2dcdc
Nadhira Saffanah Zahra <nadhirasaffanah@gmai.com>
2025-11-25 23:40:23 +0700

    commit pertama berisi folder awal keperluan projek

---
+++ src/main/java/com/example/tubesrpl/TubesRplApplication.java
@@ -0,0 +1,13 @@
+package com.example.tubesrpl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TubesRplApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TubesRplApplication.class, args);
+	}
+
+}
