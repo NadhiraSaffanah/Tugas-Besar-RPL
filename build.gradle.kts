bb7388f59701f36098f8b90e08153706278bd53b
seraf_liv <6182301090@student.unpar.ac.id>
2025-11-29 20:33:22 +0700

    Adjusted dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,6 +30,13 @@ dependencies {
 	annotationProcessor("org.projectlombok:lombok")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
+	// tambahan
+	implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
+	implementation("org.springframework.boot:spring-boot-starter-jdbc")
+	runtimeOnly("org.postgresql:postgresql")
+	compileOnly("org.projectlombok:lombok")
+    annotationProcessor("org.projectlombok:lombok")
 }
 
 tasks.withType<Test> {
