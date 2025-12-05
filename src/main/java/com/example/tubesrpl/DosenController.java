861defb48d1613cef1a9f938184749c666853f5c
seraf_liv <6182301090@student.unpar.ac.id>
2025-12-05 14:09:13 +0700

    added @SessionAttributes and @ModelAttributes

--- src/main/java/com/example/tubesrpl/DosenController.java
+++ src/main/java/com/example/tubesrpl/DosenController.java
@@ -6,16 +6,27 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.SessionAttributes;
 
-import com.example.tubesrpl.data.Tubes;
+import com.example.tubesrpl.model.Tubes;
+import com.example.tubesrpl.model.User;
 import com.example.tubesrpl.repository.TubesRepository;
+
+import jakarta.servlet.http.HttpSession;
 
 
 @Controller
+@SessionAttributes("user")
 public class DosenController {
     @Autowired 
     private TubesRepository tubesRepository;
 
+    @ModelAttribute("user") //supaya ga nerima parameter HttpSesison berkali kali
+    public User userSession(HttpSession session) {
+        return (User) session.getAttribute("user");
+    }
+    
     @GetMapping("/home-dosen")
     public String homeDosenView(Model model){
         List<Tubes> tubes = tubesRepository.findAllBySemester((long) 1);
