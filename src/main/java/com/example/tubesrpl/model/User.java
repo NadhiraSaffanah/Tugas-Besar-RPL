6d18fc0543d46d25ec3b51d825918386132cd8d9
seraf_liv <6182301090@student.unpar.ac.id>
2025-12-05 14:11:22 +0700

    Moved from "data" folder

---
+++ src/main/java/com/example/tubesrpl/model/User.java
@@ -0,0 +1,21 @@
+package com.example.tubesrpl.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor; // buat mapping kolom postgre
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    private Long id;
+    private String email;
+    private String password;
+    private String role;
+    private String nama;
+
+    // yg bisa null
+    private String npm;
+    private String noInduk;
+    private String fotoProfil;
+}
