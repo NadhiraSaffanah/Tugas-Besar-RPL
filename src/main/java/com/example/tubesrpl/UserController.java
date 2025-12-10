5a88f8dc24a96f2c27a44e997e3d58c70eef546c
Nadhira Saffanah Zahra <nadhirasaffanah@gmai.com>
2025-12-10 16:13:42 +0700

    fix html

--- src/main/java/com/example/tubesrpl/UserController.java
+++ src/main/java/com/example/tubesrpl/UserController.java
@@ -3,6 +3,7 @@ package com.example.tubesrpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import com.example.tubesrpl.repository.UserRepository;
 
 @Controller
 public class UserController {
