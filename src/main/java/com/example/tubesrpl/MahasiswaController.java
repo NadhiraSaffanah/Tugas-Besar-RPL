a4dafe636091e32203c659a421b543478f566e07
seraf_liv <6182301090@student.unpar.ac.id>
2025-12-12 22:15:29 +0700

    misc adjust

--- src/main/java/com/example/tubesrpl/MahasiswaController.java
+++ src/main/java/com/example/tubesrpl/MahasiswaController.java
@@ -6,21 +6,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.SessionAttributes;
 
 import com.example.tubesrpl.model.Phase;
 import com.example.tubesrpl.model.Tubes;
-import com.example.tubesrpl.model.User;
 import com.example.tubesrpl.repository.TubesRepository;
 
-import jakarta.servlet.http.HttpSession;
+import lombok.extern.slf4j.Slf4j;
+
 import com.example.tubesrpl.repository.PhaseRepository;
 
+@Slf4j
 @Controller
-@SessionAttributes("user")
-@RequestMapping("/mahasiswa")
 public class MahasiswaController {
     @Autowired 
     private TubesRepository tubesRepository;
@@ -28,24 +24,13 @@ public class MahasiswaController {
     @Autowired
     private PhaseRepository phaseRepository;
 
-    @ModelAttribute("user") //supaya ga nerima parameter HttpSesison berkali kali
-    public User userSession(HttpSession session) {
-        return (User) session.getAttribute("user");
-    }
-
-    @GetMapping("/home")
+    @GetMapping("/mahasiswa/home")
     public String homeMhsView(Model model){
-        System.out.println(phaseRepository);
         List<Tubes> tubes = tubesRepository.findAllBySemester((long) 1);
         List<Phase> phase = phaseRepository.findAllBySemester((long) 1);
        
         model.addAttribute("tubeslist", tubes); //sementara ambil semester 1 doang
         model.addAttribute("phaselist", phase);
         return "Mahasiswa/homeMhs";
-    }
-
-    @GetMapping("/course")
-    public String courseMhsView(){
-        return "matkul";
     }
 }
